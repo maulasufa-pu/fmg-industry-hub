10df436b29dfaebe8594f2f1a8e8c0306922f0db
Alfath Dev <43428127+MaulaSufa@users.noreply.github.com>
2025-08-08 05:11:45 +0700

    jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@
 
 /** @type {import('jest').Config} */
 const config = {
+  testEnvironment: "jsdom",
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 

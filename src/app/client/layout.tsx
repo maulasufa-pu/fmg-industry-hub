2b1227ef059645500670adf49cae5b6a95f4512c
Alfath Dev <43428127+MaulaSufa@users.noreply.github.com>
2025-08-13 15:02:22 +0700

    payment system

--- src/app/client/layout.tsx
+++ src/app/client/layout.tsx
@@ -12,7 +12,7 @@ import ClientAutoF5 from "@/components/ClientWakeReloader";
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     return (
     <RequireAuth>
-      <ClientAutoF5/>
+      {/* <ClientAutoF5/> */}
       <div className="flex items-start relative bg-coolgray-10 w-full min-h-screen">
         <SidebarSection />
         <main className="flex-1 min-w-0">{children}</main>
